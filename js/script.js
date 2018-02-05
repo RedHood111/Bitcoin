a749f08471858c5a4c8ea969a2bc47053b0dda8f
mariya <mhozyaenko@gmail.com>
2018-02-05 16:41:37 +0200

    при наведении мышью меняется номер карты//номер кошелька

--- js/script.js
+++ js/script.js
@@ -1,9 +1,11 @@
 var attr = document.querySelector('.cardNumber');
-document.addEventListener('click', function(e){
+document.addEventListener('mouseover', function(e){
     var _this=e.target;
     var status = attr.getAttribute('placeholder');
-    if (_this.matches('.changeForm')) {
-        status=='Номер карты получателя' ? attr.setAttribute('placeholder', 'Номер кошелька') : attr.setAttribute('placeholder', 'Номер карты получателя')
+    if (_this.matches('.p2')||_this.matches('.p6')) {
+        attr.setAttribute('placeholder', 'Номер карты получателя')
     }
-    
+    if (_this.matches('.p1') || _this.matches('.p3') || _this.matches('.p4') || _this.matches('.p5') || _this.matches('.p7') || _this.matches('.p8') || _this.matches('.p9')) {
+         attr.setAttribute('placeholder', 'Номер кошелька')
+    }
 })
